9584a6c274fee785fcffd3aa9f4742de682c5109
BBrzek <107590216+BBrzek@users.noreply.github.com>
2022-06-21 12:56:54 +0200

    All variables

--- trigger iban check.sql
+++ trigger iban check.sql
@@ -4,5 +4,16 @@ on bank_number
 for insert
 as
 begin
-
+	declare @acc_number varchar(max) = (select acc_number from inserted) -- inserted value of account number
+	declare @nr1 varchar(max)
+	declare @nr2 varchar(max)
+	declare @nr3 varchar(max)
+	declare @nr4 varchar(max)
+	declare @nr5 varchar(max)
+	declare @mod1 varchar(max)
+	declare @mod1nr2 varchar(max)
+	declare @mod2 varchar(max)
+	declare @mod2nr3 varchar(max)
+	declare @mod3 varchar(max)
+	declare @mod3nr4 varchar(max)
 end
